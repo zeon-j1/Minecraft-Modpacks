66f3f4cbf569cbcf407780b3772ff0578780ccd4
Nicolas A. <zeon_j1@proton.me>
2025-10-26 14:34:53 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
 
-# THIS FILE IS ONLY FOR PEOPLE INSTALLING THE MODPACK MANUALLY
-
-DATE=$(date)
-PWD=$(pwd)
-
-echo "Installation time: $DATE"
-
-
-while []
-do
-  echo "Current installtion directory: $PWD"
-done
